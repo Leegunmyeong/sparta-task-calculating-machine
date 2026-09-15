5972f674ea0a20306fd22dce17cd70fa6a43c8af
gun myeong <rirst@naver.com>
2026-09-15 21:24:38 +0900

    getter&setter 적용단계

--- src/calculator/step2/App.java
+++ src/calculator/step2/App.java
@@ -1,4 +1,65 @@
 package calculator.step2;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        int num1;
+        int num2;
+        while (true) {
+            System.out.println("====== 계산기 ======");
+            System.out.println("""
+                    계산을 위한 정수 2개를 입력해주세요😊
+                    (예: 1, 2, 100..)
+                    """);
+            while (true) {
+                System.out.print("첫 번째 숫자 입력👉 ");
+                num1 = scanner.nextInt();
+                if (num1 >= 0) {
+                    break;
+                } else {
+                    System.out.println("잘못된 값을 입력하셨습니다");
+                }
+            }
+            while (true) {
+                System.out.print("두 번째 숫자 입력👉 ");
+                num2 = scanner.nextInt();
+                if (num2 >= 0) {
+                    break;
+                } else {
+                    System.out.println("잘못된 값을 입력하셨습니다");
+                }
+            }
+            System.out.println("======================================");
+            System.out.println("""
+                    원하시는 기능을 선택해주세요👀
+                    ➕덧셈 입력기호(+)
+                    ➖뺄셈 입력기호(-)
+                    ✖️곱셈 입력기호(*)
+                    ➗나눗셈(몫, 나머지) -> 입력기호(/)
+                    """);
+            System.out.print("기호 입력(하나의 기호만 입력해주세요): ");
+            char selectCalculateFctSign = scanner.next().charAt(0);
+            calculator.calculate(num1, num2, selectCalculateFctSign);
+            System.out.println("======================================");
+            System.out.println("""
+                    계산기 종료 -> exit 입력
+                    계속 이용 -> 아무 문자나 입력
+                    """);
+            System.out.print("입력:");
+            String inputUserText = scanner.next();
+            if (inputUserText.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.🙂‍↕️");
+                break;
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
+
+
--- src/calculator/step2/Calculator.java
+++ src/calculator/step2/Calculator.java
@@ -1,11 +1,60 @@
 package calculator.step2;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Calculator {
 
     // 속성
+    // 캡슐화 private
+    // 정수 여러개를 순서대로 담는 목록 자료형
+        private List<Integer> results;
+    // 생성자
+    // 빈 리스트 생성
+        public Calculator() {
+            this.results = new ArrayList<>();
+        }
+    // 기능
+    // public int: 메서드가 모든 작업 끝내고 최종적으로 담는 값의 타입
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+            switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과: " + result);
+                break;
+            case '*':
+                System.out.println("결과: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("결과(몫): " + result);
+                }
+                break;
+            default:
+                System.out.println("잘못된 값을 입력했습니다.");
+                break;
+        }
+        // 결과를 리스트에 추가
+        results.add(result);
+        // 최종 결과값 반환
+        return  result;
+    }
 
-    //
+    public List<Integer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Integer> results) {
+        this.results = results;
+    }
 }
 
